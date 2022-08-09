f41da18468097a42e18860ae8a44b5f79ada7118
Frank Schäckermann <github-fschaeckermann@snkmail.com>
2022-08-09 23:55:08 +0200

    Extended functionality to chart categories

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # python-pptx-text-replacer
 
-A Python script using python-pptx to replace text in a PowerPoint presentation (pptx) whereever it might be.
-
-    Usage: python-pptx-text-replacer -m <match> -r <replacement> -i <input-file-name> -o <output-file-name>
-           -m and -r can be specified multiple times and are paired in the order of their appearance
+A Python script using python-pptx to replace text in a PowerPoint presentation (pptx) in:
+  - all shapes with text frames (also inside grouped shapes)
+  - chart categories
+  - table cells
 
 The script is work in progress!
 
-Changing texts in charts is not yet supported.
+There might still be quirks in PowerPoint-presentations this script doesn't deal with well. Please open an issue stating your problem and (if in any way possible) the appropriate pptx file for me to be able to re-produce your problem. If you can't include the pptx itself, maybe you can post the part of the script's output where it goes wrong.
 
-There might still be quirks in PowerPoint-presentations this script doesn't deal with well. Please open an issue stating your problem and the appropriate pptx file for me to be able to re-produce your problem.
+```
+usage: python-pptx-text-replacer.py [-h] --match <match> --replace
+                                    <replacement> --input <input file>
+                                    --output <output file> [--tables]
+                                    [--no-tables] [--charts] [--no-charts]
+                                    [--slides <list of slide numbers>]
 
-Issues without an according pptx file showing the problem can not be processed and will be closed. 
+This module implements text replacement in Powerpoint files in pptx format.
+The text is searched and replaced in all possible places.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --match <match>, -m <match>
+                        the match to look for
+  --replace <replacement>, -r <replacement>
+                        the matches' replacement
+  --input <input file>, -i <input file>
+                        the file to replace the text in
+  --output <output file>, -o <output file>
+                        the file to write the changed presentation to
+  --tables, -t          process tables as well (default)
+  --no-tables, -T       do not process tables and their cells
+  --charts, -c          process chart categories as well (default)
+  --no-charts, -C       do not process charts and their categories
+  --slides <list of slide numbers>, -s <list of slide numbers>
+                        A comma-separated list of slide numbers (1-based) to
+                        restrict processing to, i.e. '2,4,6-10'
+
+The parameters --match and --replace can be specified multiple times. They are
+paired up in the order of their appearance.
+```
--- python-pptx-text-replacer.py
+++ python-pptx-text-replacer.py
@@ -1,3 +1,8 @@
+"""
+This module implements text replacement in Powerpoint files in pptx format.
+The text is searched and replaced in all possible places.
+"""
+ 
 import sys
 import argparse
 
@@ -8,22 +13,53 @@ from pptx.enum.chart import XL_CHART_TYPE
 from pptx.enum.shapes import MSO_SHAPE_TYPE
 from pptx.util import Inches
 
-"""
-This module implements text replacement in Powerpoint files in pptx format.
-The text is searched and replaced in all possible places.
-"""
- 
 class python_pptx_text_replacer:
     """
     This class implements text replacement in Powerpoint files in pptx format.
     The text is searched and replaced in all possible places.
     """
  
-    def __init__(self, presentation_file_name):
+    def __init__(self, presentation_file_name, tables=True, charts=True, slides=''):
         self._replacements = []
         self._collected_replacements = []
         self._presentation_file_name = presentation_file_name
         self._presentation = Presentation(presentation_file_name)
+        self._tables = tables
+        self._charts = charts
+        slide_cnt = len(self._presentation.slides)
+        if len(slides.strip())==0:
+            self._slides = [ True * slide_cnt ]
+        else:
+            self._slides = [ False * slide_cnt ]
+            for r in slides.strip().split('\\s*,\\s*'):
+                range = r.split('\\s*-\\s*',3)
+                low = None
+                high = None
+                if len(range)<=2:
+                    try:
+                        low = int(range[0])
+                    except:
+                        low = None
+                    high = low
+                    if len(range)==2:
+                        if len(range[1])==0:
+                            high = slide_cnt
+                        else:
+                            try:
+                                high = int(range[1])
+                            except:
+                                high = None
+                if low is None or high is None:
+                    raise ValueError('Slide list (--slides "%s") is not a comma separated list of slide numbers (i.e. 1) or slide number ranges (i.e. 4-12)' % (slides))
+                if low<1 or low>slide_cnt:
+                    raise ValueError('Slide number %s in list (--slides "%s") is lower than 1 or bigger than the last slide number %s' % ( low, slides, slide_cnt ))
+                if high<1 or high>slide_cnt:
+                    raise ValueError('Slide number %s in list (--slides "%s") is lower than 1 or bigger than the last slide number %s' % ( high, slides, slide_cnt ))
+                if low > high:
+                    raise ValueError('Slide range %s in list (--slides "%s") is invalid.' % ( r, slides ))
+                for i in range(low-1,high):
+                    self._slides[i] = True
+
 
     def replace_text(self, replacements):
         print(self._replacements)
@@ -33,8 +69,11 @@ class python_pptx_text_replacer:
         slide_idx = 0
         print("Presentation[%s]" % (self._presentation_file_name))
         for slide in self._presentation.slides:
-            print("  Slide[%s, id=%s] with title '%s'" % ( slide_idx, slide.slide_id, "<no title>" if slide.shapes.title is None else slide.shapes.title.text ))
-            self._process_shapes(2, slide)
+            print("  Slide[%s, id=%s] with title '%s'" % ( slide_idx+1, slide.slide_id, "<no title>" if slide.shapes.title is None else slide.shapes.title.text ))
+            if self._slides[slide_idx]:
+                self._process_shapes(2, slide)
+            else:
+                print("    ... skipped")
             slide_idx += 1
 
     def write_presentation_to_file(self, presentation_output_file_name):
@@ -96,13 +135,17 @@ class python_pptx_text_replacer:
                     if pos+match_len < olen:
                         # our match ends before the end of the text of this run therefore
                         # we put the rest of our replacement string here and we are done!
+                        font = deepcopy(run.font)
                         run.text = otext[0:pos]+to_replace+otext[pos+match_len:]
+                        run.font = font
                         print("%sRun[%s,%s]: '%s' -> '%s'" % ( "  "*level, paragraph_idx, i, otext, run.text ))
                         return ('','')
                     if pos+match_len == olen:
                         # our match ends together with the text of this run therefore
                         # we put the rest of our replacement string here and we are done!
+                        font = deepcopy(run.font)
                         run.text = otext[0:pos]+to_replace
+                        run.font = font
                         print("%sRun[%s,%s]: '%s' -> '%s'" % ( "  "*level, paragraph_idx, i, otext, run.text ))
                         return ('','')
                     # we still haven't found all of our original match string
@@ -125,7 +168,9 @@ class python_pptx_text_replacer:
                         to_replace = to_replace[part_match_len:]
                         repl_len -= part_match_len
                     print("%sRun[%s,%s]: '%s' -> '%s'" % ( "  "*level, paragraph_idx, i, otext, ntext ))
+                    font = deepcopy(run.font)
                     run.text = ntext            # save the new text to the run
+                    run.font = font             # restore font settings for run
                     to_match = to_match[part_match_len:] # this is what is left to match
                     match_len -= part_match_len # this is the length of the match that is left
                     pos = 0                     # in the next run, we start at pos 0 with our match
@@ -155,20 +200,103 @@ class python_pptx_text_replacer:
                 row_cnt = len(table.rows)
                 col_cnt = len(table.columns)
                 print("%sTable[%s,%s]" % ( "  "*(level+1), row_cnt, col_cnt ) )
-                for row in range(0, row_cnt):
-                    for col in range(0, col_cnt):
-                        cell = table.cell(row,col)
-                        print("%sCell[%s,%s]: '%s'" % ( "  "*(level+2), row, col, cell.text ))
-                        self._process_text_frame(level+3, cell.text_frame)
+                if self._tables:
+                    for row in range(0, row_cnt):
+                        for col in range(0, col_cnt):
+                            cell = table.cell(row,col)
+                            print("%sCell[%s,%s]: '%s'" % ( "  "*(level+2), row, col, cell.text ))
+                            self._process_text_frame(level+3, cell.text_frame)
+                else:
+                    print("%s... skipped" % ( "  "*(level+2)))
             if shape.shape_type==MSO_SHAPE_TYPE.GROUP:
                 self._process_shapes(level+1, shape)
+            if shape.has_chart:
+                chart = shape.chart
+                print("%sChart of type %s" % ( "  "*(level+1), chart.chart_type ) )
+                if self._charts:
+                    categories_changed = False
+                    new_categories = []
+                    category_idx = 0
+                    for category in chart.plots[0].categories:
+                        print("%sCategory[%s] '%s'" % ( "  "*(level+2), category_idx, category ))
+                        for (match,replace) in self._replacements:
+                            changed_category = category.replace(match,replace)
+                            if changed_category == category:
+                                print("%sReplacing '%s' -> no match" % ( "  "*(level+3), match ))
+                            else:
+                                print("%sReplacing '%s' -> changed to '%s'" % ( "  "*(level+3), match, changed_category ))
+                                category = changed_category
+                                categories_changed = True
+                        new_categories.append(category)
+                        category_idx += 1
+
+                    if categories_changed:
+                        new_chart_data = CategoryChartData()
+                        new_chart_data.categories = new_categories
+                        for series in chart.series:
+                            new_chart_data.add_series(series.name,series.values)
+                        chart.replace_data(new_chart_data)
+                else:
+                    print("%s... skipped" % ( "  "*(level+2)))
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument('--match',   '-m', action='append', required=True, dest='matches', metavar='<match>', help='the match to look for')
-    p.add_argument('--replace', '-r', action='append', required=True, dest='replacements', metavar='<replacement>', help="the matches' replacement")
-    p.add_argument('--input',   '-i', action='store',  required=True, metavar='<input file>', help="the file to replace the text in")
-    p.add_argument('--output',  '-o', action='store',  required=True, metavar='<output file', help="the file to write the changed presentation to")
+    p = argparse.ArgumentParser(description=__doc__, epilog="The parameters --match and --replace can be specified multiple times. They are paired up in the order of their appearance.")
+    p.add_argument('--match',   '-m',
+                   action='append',
+                   required=True,
+                   dest='matches',
+                   metavar='<match>',
+                   help='the match to look for')
+    p.add_argument('--replace', '-r',
+                   action='append',
+                   required=True,
+                   dest='replacements',
+                   metavar='<replacement>',
+                   help="the matches' replacement")
+    p.add_argument('--input',   '-i',
+                   action='store',
+                   required=True,
+                   metavar='<input file>',
+                   help="the file to replace the text in")
+    p.add_argument('--output',  '-o',
+                   action='store',
+                   required=True,
+                   metavar='<output file>',
+                   help="the file to write the changed presentation to")
+    p.add_argument('--tables',  '-t',
+                   action='store_const',
+                   dest='tables',
+                   const=True,
+                   required=False,
+                   default=True,
+                   help="process tables as well (default)")
+    p.add_argument('--no-tables','-T',
+                   action='store_const',
+                   dest='tables',
+                   const=False,
+                   required=False,
+                   default=True,
+                   help="do not process tables and their cells")
+    p.add_argument('--charts',  '-c',
+                   action='store_const',
+                   dest='charts',
+                   const=True,
+                   required=False,
+                   default=True,
+                   help="process chart categories as well (default)")
+    p.add_argument('--no-charts','-C',
+                   action='store_const',
+                   dest='charts',
+                   const=False,
+                   required=False,
+                   default=True,
+                   help="do not process charts and their categories")
+    p.add_argument('--slides', '-s',
+                   metavar='<list of slide numbers>',
+                   action='store',
+                   required=False,
+                   default='',
+                   help="A comma-separated list of slide numbers (1-based) to restrict processing to, i.e. '2,4,6-10'")
 
     ns = p.parse_args(sys.argv[1:])
 
@@ -178,7 +306,7 @@ if __name__ == '__main__':
 
     print(ns)
 
-    replacer = python_pptx_text_replacer(ns.input)
+    replacer = python_pptx_text_replacer(ns.input,tables=ns.tables,charts=ns.charts,slides=ns.slides)
     replacements = []
     for m in range(0,len(ns.matches)):
         replacements.append( ( ns.matches[m], ns.replacements[m] ) )
